942fb8e86bf8397b3274c1802a20d52edb51945d
RobbanBob <robekarlstrom@gmail.com>
2022-04-05 14:51:45 +0200

    footer adna pp.css updated

--- src/Components/Layout/Footer.jsx
+++ src/Components/Layout/Footer.jsx
@@ -6,22 +6,23 @@ import {
 } from 'react-bootstrap';
 import { FaFacebookSquare, FaInstagramSquare } from "react-icons/fa";
 
+
 const Footer = () => {
   return (
     <footer className='text-center text-dark'  style={{ backgroundColor: '#f1f1f1' }}>
       <Container className='pt-4'>
         <section className='mb-4'>
         
-            <FaFacebookSquare/>
+            <FaFacebookSquare className="footer-icons"/>
         
-            <FaInstagramSquare/>
+            <FaInstagramSquare className="footer-icons"/>
 
         </section>
       </Container>
 
-      <div className='text-center text-dark p-3 display-6' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-        © 2020 Copyright:
-        <a className='text-dark' href='https://mdbootstrap.com/'>
+      <div className='footer-text' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        © 2022 Copyright:
+        <a className='footer-link' href='https://mdbootstrap.com/'>
           Githublänk här
         </a>
       </div>
