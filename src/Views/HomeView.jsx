3864345eb65ae48c778e0f4605c7a1d38c0b8dd9
mattez1992 <matte_920319@live.se>
2022-04-05 13:12:32 +0200

    Started work with home view

--- src/Views/HomeView.jsx
+++ src/Views/HomeView.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
-
+import React,{useState, useEffect} from 'react'
+import axios from 'axios'
+import { v4 as uuidv4 } from "uuid";
 const HomeView = () => {
+    const [auctions, setAuctions] = useState([]);
+    useEffect(() => {
+        const getAuctions = async ()=>{
+            const response = await axios.get("http://nackowskis.azurewebsites.net/api/Auktion/2460"
+            )
+            setAuctions(response.data);
+        }
+        getAuctions();
+    }, []);
   return (
-    <div><h1>Home view</h1></div>
+    <div><h1>Home view</h1>
+        {auctions.length > 0 ? auctions.map(a => (
+            <h2 key={uuidv4()}>{a.Titel}</h2>
+        )) : <h2>No Auctions found</h2>}
+    
+    </div>
   )
 }
 
