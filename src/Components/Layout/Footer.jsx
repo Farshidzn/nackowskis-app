68424ae09670dc12e36ff8aa71181ff6cdc43f32
RobbanBob <robekarlstrom@gmail.com>
2022-04-05 14:31:54 +0200

    footer updated

--- src/Components/Layout/Footer.jsx
+++ src/Components/Layout/Footer.jsx
@@ -4,37 +4,25 @@ import {
   Col,
   Row
 } from 'react-bootstrap';
-import { ImFacebook2, FaInstagramSquare } from "react-icons/gr";
+import { FaFacebookSquare, FaInstagramSquare } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className='text-center text-white' style={{ backgroundColor: '#f1f1f1' }}>
+    <footer className='text-center text-dark'  style={{ backgroundColor: '#f1f1f1' }}>
       <Container className='pt-4'>
         <section className='mb-4'>
-          <a
-            className='btn btn-link btn-floating btn-lg text-dark m-1'
-            href='#!'
-            role='button'
-            data-mdb-ripple-color='dark'
-          >
-            <ImFacebook2 fab className='fab fa-facebook-f' />
-          </a>
+        
+            <FaFacebookSquare/>
+        
+            <FaInstagramSquare/>
 
-          <a
-            className='btn btn-link btn-floating btn-lg text-dark m-1'
-            href='#!'
-            role='button'
-            data-mdb-ripple-color='dark'
-          >
-            <FaInstagramSquare fab className='fa-instagram' />
-          </a>
         </section>
       </Container>
 
-      <div className='text-center text-dark p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+      <div className='text-center text-dark p-3 display-6' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
         © 2020 Copyright:
         <a className='text-dark' href='https://mdbootstrap.com/'>
-          MDBootstrap.com
+          Githublänk här
         </a>
       </div>
     </footer>
