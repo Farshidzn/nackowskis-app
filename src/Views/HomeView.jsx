8f56b92588d0aa02dce37bfa76abbf6e49a36c23
mattez1992 <matte_920319@live.se>
2022-04-05 13:52:41 +0200

    Created AuctionItem

--- src/Views/HomeView.jsx
+++ src/Views/HomeView.jsx
@@ -1,23 +1,12 @@
-import React,{useState, useEffect} from 'react'
-import axios from 'axios'
-import { v4 as uuidv4 } from "uuid";
+import React from 'react'
+
+import AuctionList from '../Components/Auctions/AuctionList';
 const HomeView = () => {
-    const [auctions, setAuctions] = useState([]);
-    useEffect(() => {
-        const getAuctions = async ()=>{
-            const response = await axios.get("http://nackowskis.azurewebsites.net/api/Auktion/2460"
-            )
-            setAuctions(response.data);
-        }
-        getAuctions();
-    }, []);
+  
   return (
-    <div><h1>Home view</h1>
-        {auctions.length > 0 ? auctions.map(a => (
-            <h2 key={uuidv4()}>{a.Titel}</h2>
-        )) : <h2>No Auctions found</h2>}
-    
-    </div>
+    <>
+        <AuctionList />
+    </>
   )
 }
 
