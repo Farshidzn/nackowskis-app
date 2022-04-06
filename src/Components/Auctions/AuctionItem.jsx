02a5c2d4274bf8bc2f1878cfddea201a960c79c6
RobbanBob <robekarlstrom@gmail.com>
2022-04-06 15:12:38 +0200

    auction item date fix

--- src/Components/Auctions/AuctionItem.jsx
+++ src/Components/Auctions/AuctionItem.jsx
@@ -2,14 +2,29 @@ import React from "react";
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 const AuctionItem = ({ auction }) => {
+
+  const dateConverter = (d) => {
+    return new Date(d);
+  }
+
+  const statusChecker = (endDate) => {
+    let currentDate = new Date();
+
+    if(currentDate > dateConverter(endDate))
+    {
+      return "Avslutad"
+    }
+    else{
+      return "Slut Datum: " + new Date(endDate).toISOString().split("T")[0]
+    }
+  }
+
   return (
     <Card className="my-2">
       <Card.Body>
         <Card.Title>{auction.Titel}</Card.Title>
         <Card.Text>{auction.Beskrivning}</Card.Text>
-        <Card.Text>
-          Slut Datum: {new Date(auction.SlutDatum).toISOString().split("T")[0]}
-        </Card.Text>
+          <Card.Text>{statusChecker(auction.SlutDatum)}</Card.Text>
       </Card.Body>
       <Link
         to={`/auction/${auction.AuktionID}`}
