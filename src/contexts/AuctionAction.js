04221d92f24c33f956b9d4cf6d206a63696d3584
RobbanBob <robekarlstrom@gmail.com>
2022-04-07 10:57:59 +0200

    Merge branch 'master' of https://github.com/Farshidzn/nackowskis-app

--- src/contexts/AuctionAction.js
+++ src/contexts/AuctionAction.js
@@ -7,47 +7,65 @@ const dateConverter = (d) => {
     return new Date(d);
 }
 
+
+
 export const searchFilter = async (condition, status) => {
 
     const response = await api.get("Auktion/2460");
     const {data} = response;
-    const currentDate = new Date()
+    let currentDate = new Date();
     let filteredList = []
 
     switch(status){
-
+        
         case "active": 
            data.forEach(auctionItem => {
                if(auctionItem.Titel.includes(condition) && dateConverter(auctionItem.SlutDatum) > currentDate) {
                    filteredList.push(auctionItem)
-               } 
-           }); break;
+               }
+           });
+           filteredList.sort((auction1, auction2) => dateConverter(auction1.SlutDatum) - dateConverter(auction2.SlutDatum))
+           break;
             
         case "closed":
             data.forEach(auctionItem => {
                 if(auctionItem.Titel.includes(condition) && dateConverter(auctionItem.SlutDatum) < currentDate) {
                     filteredList.push(auctionItem)
                 } 
-            }); break;
+                
+            });
+            filteredList.sort((auction1, auction2) => dateConverter(auction1.SlutDatum) - dateConverter(auction2.SlutDatum))
+            break;
         
-        case "all":
+        /*case "all":
                 data.forEach(auctionItem => {
                     if(auctionItem.Titel.includes(condition)){
                         filteredList.push(auctionItem)
-                    } 
-                }); break;
+                    }
+                    filteredList.sort((auction1, auction2) => dateConverter(auction1.SlutDatum) - dateConverter(auction2.SlutDatum))
+                }); break;*/
         default:
             data.forEach(auctionItem => {
                 if(auctionItem.Titel.includes(condition)){
                     filteredList.push(auctionItem)
-                } 
-            }); break;
+                }   
+            });
+            filteredList.sort((auction1, auction2) => dateConverter(auction1.SlutDatum) - dateConverter(auction2.SlutDatum))
+            
+                let closedList = [];
+                let activeList = [];
+                filteredList.forEach(auctionItem => {
+                    if(dateConverter(auctionItem.SlutDatum) < currentDate)
+                    {
+                        closedList.push(auctionItem)
+                    }
+                    else
+                    {
+                        activeList.push(auctionItem)
+                    }            
+                })
+                filteredList = activeList.concat(closedList);
+            break;
     }
-
-  
-
-  
-
     return filteredList;
-
 }
