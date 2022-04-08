524c9394207ac50fe91aeb3a3ef62853fcd05198
DESKTOP-1CL2VLB\matte <matte_920319@live.se>
2022-04-08 15:31:33 +0200

    Fixed minor styling in footer

--- src/Components/Layout/Footer.jsx
+++ src/Components/Layout/Footer.jsx
@@ -1,25 +1,52 @@
-import React from 'react';
-import { Container} from 'react-bootstrap';
+import React from "react";
+import { Container, Col, Row } from "react-bootstrap";
 import { FaFacebookSquare, FaInstagramSquare } from "react-icons/fa";
-
 
 const Footer = () => {
   return (
-    <footer className='text-center text-dark'  style={{ backgroundColor: '#f1f1f1' }}>
-      <Container className='pt-4'>
-        <section className='mb-4'>
-            <a href="https://www.facebook.com/nackorad"><FaFacebookSquare className="footer-icons"/></a>
-            <a href="https://www.instagram.com/pawe_n/"><FaInstagramSquare className="footer-icons"/></a>
-        </section>
-      </Container>
+    <footer className="text-center text-dark mt-3">
+      <Container fluid>
+        <Row
+          className="visable py-2"
+          style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+        >
+          <Col>
+            <a
+              href="https://www.facebook.com/nackorad"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <FaFacebookSquare className="footer-icons" />
+            </a>
+            <a
+              href="https://www.instagram.com/pawe_n/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <FaInstagramSquare className="footer-icons" />
+            </a>
+          </Col>
+        </Row>
 
-      <div className='footer-text' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-        © 2022 Copyright:
-        <a className='footer-link' href='https://mdbootstrap.com/'>
-          Githublänk här
-        </a>
-      </div>
+        <Row
+          className="footer-text"
+          style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+        >
+          <Col>
+            {" "}
+            © 2022 Copyright:
+            <a
+              className="footer-link"
+              href="https://mdbootstrap.com/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              Githublänk här
+            </a>
+          </Col>
+        </Row>
+      </Container>
     </footer>
   );
-}
+};
 export default Footer;
